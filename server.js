a21256b8bc4794b6665e6c092860a9a41af0e148
Amos .E <anjeas4@gmail.com>
2017-05-13 14:56:22 +0100

    complete a working version

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-    res.send("Hello World");
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+app.use('/', function(req, res){
+	// get the date string appended to the URL
+    var dateString = req.originalUrl.slice(1);
+
+    // confirm not empty string or null recieved
+    if((dateString != null) && (dateString.length > 0)){
+    	var date = decodeURI(dateString);
+    	var uniTimestamp = parseInt(date);
+    	if(uniTimestamp){
+    		date = new Date(uniTimestamp);
+    	} else {
+    		var naturalDate = Date.parse(date);
+    		date = new Date(naturalDate);
+    	}
+
+    	var humanDate = monthNames[date.getMonth()] + " " + date.getDay() + ", " + date.getFullYear();
+    	
+    }
+
+    res.send(JSON.stringify({ "unix":  Date.parse(date), "natural":  humanDate }));
+
 });
 
-app.listen(8080, function(){
-    console.log("sever started on port 8080");
+app.listen(3000, function(){
+    console.log("sever started on port 3000");
 })
